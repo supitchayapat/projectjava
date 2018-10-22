7b36fee1c10e7a5a1141948ea4297d1936acc7a3
supitchayapat <sailabaht.sal@ku.th>
2018-10-22 13:48:00 +0700

    import

--- src/sample/controller/InformationController.java
+++ src/sample/controller/InformationController.java
@@ -2,7 +2,22 @@ package controller;
 
 import javafx.scene.input.MouseEvent;
 
-public class InformationController {
+public class InformationController implements INTER {
+
+
+
+    private static InformationController informationControllerInstance;
+
+    private InformationController() {
+    }
+
+    public static InformationController getInstance() {
+        if (informationControllerInstance == null) {
+            informationControllerInstance = new InformationController();
+        }
+        return informationControllerInstance;
+    }
+
     public void rightRadioCourse(MouseEvent mouseEvent) {
     }
 
